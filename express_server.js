00132f7b90ffedc53facc7d3efcc69ab17259b6f
tonyloba <tonyloba@gmail.com>
2021-09-28 20:14:45 +0000

    add delete and installed nodemon

--- express_server.js
+++ express_server.js
@@ -8,35 +8,59 @@ app.use(bodyParser.urlencoded({extended: true}));
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
-// index page
+// Index page:
+
 const urlDatabase = {
    "b2xVn2": {longURL: "http://www.lighthouselabs.ca"},
-   "9sm5xK": {longURL: "http://www.google.com"}
+   "9sm5xK": {longURL: "http://www.google.com"},
+   "bnun15": {longURL: "http://www.tweeter.com"}
 };
 
-//Create page to make new URLs
+//Create page to make new URLs:
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// submit data POST with generated shortURL, adds it to urlDatabase  and redirects to "/urls/:shortURL"
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  //console.log(req.body);  // Log the POST request body to the console
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 
+//New ShortURL pages
 
-// Create page for created shortURL:
+// Create page for newly created shortURL:
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL};
   res.render("urls_show", templateVars);
 });
 
+// endpoint "/u/:shortURL" will redirect to its longURL
+app.get("/u/:shortURL", (req, res) => {
+   const longURL = urlDatabase[req.params.shortURL].longURL
+  res.redirect(longURL);
+  
+});
 
 
+///Home page render
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 
+// Delete URL pages :
+app.post("/urls/:shortURL/delete", (req, res) => {
 
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
+
+})
 
 
 app.listen(PORT, () => {
@@ -46,9 +70,9 @@ app.listen(PORT, () => {
 
 
 
-// generator 6 symbols string :
+// generator : 6 symbol's string :
 const generateRandomString = function() {
-  const str = (Math.random()+1).toString(36).substring(7);
+  const str = Math.random().toString(36).substring(2,8);
   return str;
 }
 
